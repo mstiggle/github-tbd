2257c0bb479c4f3a002523a16a6a8ec1b9b5bbf8
Maggie Stiggleman <mstiggleman@gmail.com>
2020-07-13 14:42:34 -0400

    Changed to cyan

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: 'cyan',
     alignItems: 'center',
     justifyContent: 'center',
   },
